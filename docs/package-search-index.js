6ca7879cdc6e58d2e88684a3f8af54c8aa25ec9b
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2023-07-06 12:18:07 +0000

    Ajout du dossier 'docs' pour la documentation API

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Mondepackage"},{"l":"NettoyeurDistraitpackage"},{"l":"PollueurSauteurpackage"},{"l":"PollueurToutDroitpackage"},{"l":"RobotNettoyeurpackage"},{"l":"Robotpackage"},{"l":"Robotpollueurpackage"}];updateSearchResults();
